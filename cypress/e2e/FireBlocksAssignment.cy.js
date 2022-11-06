13c5b4c2e375451709bd4a61addf864b5ab9fd0d
AlexeyBen <alexey.ben@gmail.com>
2022-11-06 11:22:35 +0200

    added input formula functions

--- cypress/e2e/FireBlocksAssignment.cy.js
+++ cypress/e2e/FireBlocksAssignment.cy.js
@@ -11,62 +11,60 @@ beforeEach(() => {
 describe('Alexey Ben`s home assignment from fireblocks. I found three possible ways to validate the result of the equations and decided to show all of them', () => {
     it('Validation of mathematical  formula: "4 + 1 = 5" ', function () {
         const expectedResult = 4 + 1
-        cy.get(calc.numbers.four).click()
-        cy.get(calc.operators.plus).click()
-        cy.get(calc.numbers.one).click()
-        cy.intercept('POST', Cypress.config().baseUrl + "/calc").as('getRoute');
-        cy.get(calc.operators.equal).click()
-        cy.wait('@getRoute').then(interception => {
-            const data = interception.response.body
-            cy.wrap(data['results']['0']['out']).should('include', expectedResult)
+        inputFormulaWithInterception(["four", "plus", "one"], {
+            requestType: "POST",
+            requestUrl: "/calc",
+            expectedResult
         })
     });
 
 
     it('Validation of mathematical  formula: "12 - 6 = 6" with the second possible way - double click on the equal sign', function () {
         const expectedResult = 12 - 6
-        cy.get(calc.numbers.one).click()
-        cy.get(calc.numbers.two).click()
-        cy.get(calc.operators.minus).click()
-        cy.get(calc.numbers.six).click()
-        cy.get(calc.operators.equal).dblclick()
+        inputFormula(["one", "two", "minus", "six"], true)
         cy.get(calc.result).invoke('attr', 'title').should("equal", expectedResult.toString())
     });
 
     it('Validation of mathematical  formula: "(14 - 2) * 2 != 20" with the third option of validation', function () {
         const expectedResult = 20
-        cy.get(calc.left_bracket).click()
-        cy.get(calc.numbers.one).click()
-        cy.get(calc.numbers.four).click()
-        cy.get(calc.operators.minus).click()
-        cy.get(calc.numbers.two).click()
-        cy.get(calc.right_bracket).click()
-        cy.get(calc.operators.multiply).click()
-        cy.get(calc.numbers.two).click()
-        cy.get(calc.operators.equal).click()
-        selectFromListOfElement({element : calc.history.history_dropdown , index: 1}).click()
-        cy.get(calc.history.history_frame_result).invoke('attr' , 'title').should("not.equal", expectedResult.toString())
-
+        inputFormula(["left_bracket", "one", "four", "minus", "two", "right_bracket", "multiply", "two"])
+        selectFromListOfElement({element: calc.history_dropdown, index: 1}).click()
+        cy.get(calc.history_frame_result).invoke('attr', 'title').should("not.equal", expectedResult.toString())
     });
 
     it(' Validation of mathematical  formula:"sin(30) = 0.5"', function () {
-        const expectedResult = Math.round(Math.sin(30 * Math.PI / 180.0)*10)/10
-        cy.get(calc.sinus).click()
-        cy.get(calc.numbers.three).click()
-        cy.get(calc.numbers.zero).click()
-        cy.get(calc.right_bracket).click()
-        cy.get(calc.operators.equal).dblclick()
+        const expectedResult = Math.round(Math.sin(30 * Math.PI / 180.0) * 10) / 10
+        inputFormula(["sinus", "three", "zero", "right_bracket"], true)
         cy.get(calc.result).invoke('attr', 'title').should("equal", expectedResult.toString())
     });
 
     it('Validation of history - will validate more than 4 options in history dropdown because of my solutions', function () {
-        selectFromListOfElement({element : calc.history.history_dropdown , index: 1}).click()
-        cy.get(calc.history.history_frame_result).should('have.length.at.least' , 4)
-
+        selectFromListOfElement({element: calc.history_dropdown, index: 1}).click()
+        cy.get(calc.history_frame_result).should('have.length.at.least', 4)
     });
 
+    const selectFromListOfElement = ({element, index}) => {
+        return cy.get(element).eq(index)
+    }
 
-    const selectFromListOfElement = ({element , index}) => {
-         return cy.get(element).eq(index)
+    // no need to provide the equal sign as part of the formula array.
+    const inputFormula = (formula, doubleClickOnEqual = false) => {
+        for (let i = 0; i < formula.length; i++) {
+            cy.get(calc[formula[i]]).click()
+        }
+        doubleClickOnEqual ? cy.get(calc.equal).dblclick() : cy.get(calc.equal).click()
+    }
+
+    // no need to provide the equal sign as part of the formula array.
+    const inputFormulaWithInterception = (formula, {requestType, requestUrl, expectedResult}) => {
+        for (let i = 0; i < formula.length; i++) {
+            cy.get(calc[formula[i]]).click()
+        }
+        cy.intercept(requestType, Cypress.config().baseUrl + requestUrl).as('getRoute');
+        cy.get(calc.equal).click()
+        cy.wait('@getRoute').then(interception => {
+            const data = interception.response.body
+            cy.wrap(data['results']['0']['out']).should('include', expectedResult)
+        })
     }
 })
