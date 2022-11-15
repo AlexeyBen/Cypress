8bd7797e2cb55282b78aebd56b10f6d62e92a62e
AlexeyBen <alexey.ben@gmail.com>
2022-11-15 17:29:59 +0200

    cleared comments

--- cypress/e2e/WebCalc.cy.js
+++ cypress/e2e/WebCalc.cy.js
@@ -15,23 +15,27 @@ describe('I found three possible ways to validate the result of the equations an
 
     it('Validation of mathematical  formula: "4 + 1 = 5" ', function () {
         const expectedResult = 4 + 1
-        mp.formula({equation: [mp.elements.four() , mp.elements.plus() , mp.elements.one()]})
+        mp.formula({equation: [mp.elements.four(), mp.elements.plus(), mp.elements.one()]})
         cy.wait('@getRoute').then(interception => {
-                const data = interception.response.body
-                cy.wrap(data['results']['0']['out']).should('include', expectedResult)
-            })
+            const data = interception.response.body
+            cy.wrap(data['results']['0']['out']).should('include', expectedResult)
+        })
     });
 
 
     it('Validation of mathematical  formula: "12 - 6 = 6" with the second possible way - double click on the equal sign', function () {
         const expectedResult = 12 - 6
-        mp.formula({equation: [mp.elements.one(), mp.elements.two() , mp.elements.minus() , mp.elements.six()] , doubleClickEqual : true })
+        mp.formula({
+            equation: [mp.elements.one(), mp.elements.two(), mp.elements.minus(), mp.elements.six()],
+            doubleClickEqual: true
+        })
         mp.elements.result().invoke('attr', 'title').should("equal", expectedResult.toString())
     });
 
     it('Validation of mathematical  formula: "(14 - 2) * 2 != 20" with the third option of validation', function () {
         const expectedResult = 20
-        mp.formula({equation:
+        mp.formula({
+            equation:
                 [
                     mp.elements.left_bracket(),
                     mp.elements.one(),
@@ -41,14 +45,18 @@ describe('I found three possible ways to validate the result of the equations an
                     mp.elements.right_bracket(),
                     mp.elements.multiply(),
                     mp.elements.two(),
-                ]})
+                ]
+        })
         selectFromListOfElement({element: mp.elements.history_dropdown(), index: 1}).click()
         cy.get(mp.elements.history_frame_result).invoke('attr', 'title').should("not.equal", expectedResult.toString())
     });
 
     it(' Validation of mathematical  formula:"sin(30) = 0.5"', function () {
         const expectedResult = Math.round(Math.sin(30 * Math.PI / 180.0) * 10) / 10
-       mp.formula({equation: [mp.elements.sinus() , mp.elements.three() , mp.elements.zero() , mp.elements.right_bracket()] , doubleClickEqual : true})
+        mp.formula({
+            equation: [mp.elements.sinus(), mp.elements.three(), mp.elements.zero(), mp.elements.right_bracket()],
+            doubleClickEqual: true
+        })
         mp.elements.result().invoke('attr', 'title').should("equal", expectedResult.toString())
     });
 
--- cypress/pages/MainPage.js
+++ cypress/pages/MainPage.js
@@ -25,45 +25,12 @@ class MainPage {
         this.elements.clearBtn().click()
     }
 
-    plusFunc = ({num1, num2} , doubleClickEqual = false) => {
-        num1.click()
-        this.elements.plus().click()
-        num2.click()
-        doubleClickEqual ? this.elements.equal().dblclick() : this.elements.equal().click()
-    }
-
-
-
-    minusFunc = ({num1, num2},doubleClickEqual = false ) => {
-        num1.click()
-        this.elements.minus().click()
-        num2.click()
-        doubleClickEqual ? this.elements.equal().dblclick() : this.elements.equal().click()
-    }
-
-
-    formula = ({equation , doubleClickEqual = false}) => {
-        equation.forEach( element => {
+    formula = ({equation, doubleClickEqual = false}) => {
+        equation.forEach(element => {
             element.click()
         })
         doubleClickEqual ? this.elements.equal().dblclick() : this.elements.equal().click()
     }
-    // // no need to provide the equal sign as part of the formula array.
-    // inputFormulaWithInterception = ({formula}, {requestType, requestUrl, expectedResult}) => {
-    // formula.forEach( arrayElement => {
-    //     console.log(arrayElement)
-    //     this.elements['four'].click()
-    // })
-    // // for (let i = 0; i < formula.length; i++) {
-    // //     this.elements[formula[i]].click()
-    // // }
-    // cy.intercept(requestType, Cypress.config().baseUrl + requestUrl).as('getRoute');
-    // this.elements.equal.click()
-    // cy.wait('@getRoute').then(interception => {
-    //     const data = interception.response.body
-    //     cy.wrap(data['results']['0']['out']).should('include', expectedResult)
-    // })
-
 }
 
 module.exports = new MainPage()
